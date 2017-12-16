efc67d043d2b971bfa325e8f8124be0865cb09d3
Serge <vspox@yandex.ru>
2017-12-17 00:25:29 +0200

    initial commit

---
+++ dist/js/slider-settings.js
@@ -0,0 +1,10 @@
+    $(document).ready(function(){
+        $('.grid-container__slider').slick({
+            dots: true,
+            infinite: true,
+            slide: "span",
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        });
+    });
